e3b151068bcb6390700cb7ced2d9b04e717cddbd
Nick Cacace <NickCacace@gmail.com>
2020-01-25 14:09:08 -0500

    chore: add prettier

--- server.js
+++ server.js
@@ -10,12 +10,12 @@ app.use(morgan('common'));
 app.use(compression());
 
 app.get('*', function(req, res, next) {
-	if (
-		req.headers['x-forwarded-proto'] !== 'https' &&
-		process.env.NODE_ENV === 'production'
-	)
-		res.redirect(301, `https://${req.hostname}${req.url}`);
-	else next(); /* Continue to other routes if we're not redirecting */
+  if (
+    req.headers['x-forwarded-proto'] !== 'https' &&
+    process.env.NODE_ENV === 'production'
+  )
+    res.redirect(301, `https://${req.hostname}${req.url}`);
+  else next(); /* Continue to other routes if we're not redirecting */
 });
 
 app.use('/', express.static(path.join(__dirname, 'build')));
