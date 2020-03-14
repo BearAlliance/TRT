6f8b01f0f0022ee132e00673523b88cf06ca2d80
Nick Cacace <NickCacace@gmail.com>
2020-03-14 19:16:36 -0400

    add fit page

--- src/pages/fit/fit-page.jsx
+++ src/pages/fit/fit-page.jsx
@@ -1,5 +1,28 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import './fit-page.scss';
 
 export default function FitPage() {
-  return <div>Fit page</div>;
+  const fitText =
+    'Precision Fit is a comprehensive analysis of the interface between cyclist and bicycle. Using our motion capture fitting system we aim to optimize the interface between two and make you a more comfortable and more efficient Cyclist. Popper fit on your bicycle will be confidence inspiring and enhance your overall cycling experience. Fits are performed weekly by appointment.';
+  return (
+    <Fragment>
+      <div className="hero fit-banner">
+        <div className="container">
+          <div className="hero-body has-text-secondary fit-text">
+            <div className="column is-4 is-invisible-touch">{fitText}</div>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <section className="tile is-vertical is-parent  is-invisible-desktop">
+          <div className="tile is-child box">{fitText}</div>
+        </section>
+        <div className="hero">
+          <div className="hero-body">
+            <h1 className="brand-font title">Fit Pricing</h1>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
 }
