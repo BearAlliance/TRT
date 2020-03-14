f36d6fc454d7e4862489d7e0563abc07f577e027
Nick Cacace <NickCacace@gmail.com>
2020-03-13 21:40:19 -0400

    add image to home page

--- src/pages/home/home-page.jsx
+++ src/pages/home/home-page.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import './home-page.scss';
 
 export default function HomePage() {
-  return <div>Home page</div>;
+  return (
+    <div className="home-page">
+      <section className="hero">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title splash-text">TRT Bicycles</h1>
+            <h2 className="subtitle">Family owned. Rider focused.</h2>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
