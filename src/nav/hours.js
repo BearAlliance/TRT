aba69365041bdb395587c3c6b85325d5e0a5bf33
Nick Cacace <NickCacace@gmail.com>
2020-03-15 17:50:53 -0400

    update to spring hours

--- src/nav/hours.js
+++ src/nav/hours.js
@@ -2,16 +2,16 @@ const weekDay = new Date().getDay();
 
 const hours = {
   spring: {
-    Monday: '11:00AM - 6:00PM',
-    tuesday: 'CLOSED',
-    wednesday: '9:00AM - 6:00PM',
-    thursday: '9:00AM - 6:00PM',
-    friday: '9:00AM - 6:00PM',
-    saturday: '9:00AM - 4:00PM',
-    sunday: '9:00AM - 4:00PM'
+    monday: '11:00AM - 6:00PM',
+    tuesday: '11:00AM - 6:00PM',
+    wednesday: '11:00AM - 6:00PM',
+    thursday: '11:00AM - 6:00PM',
+    friday: '11:00AM - 6:00PM',
+    saturday: '11:00AM - 4:00PM',
+    sunday: 'CLOSED'
   },
   summer: {
-    Monday: '11:00AM - 6:00PM',
+    monday: '11:00AM - 6:00PM',
     tuesday: 'CLOSED',
     wednesday: '11:00AM - 6:00PM',
     thursday: '11:00AM - 6:00PM',
@@ -20,7 +20,7 @@ const hours = {
     sunday: '11:00AM - 5:00PM'
   },
   fall: {
-    Monday: '11:00AM - 6:00PM',
+    monday: '11:00AM - 6:00PM',
     tuesday: 'CLOSED',
     wednesday: '11:00AM - 6:00PM',
     thursday: '11:00AM - 6:00PM',
@@ -29,7 +29,7 @@ const hours = {
     sunday: '11:00AM - 5:00PM'
   },
   winter: {
-    Monday: '11:00AM - 6:00PM',
+    monday: '11:00AM - 6:00PM',
     tuesday: 'CLOSED',
     wednesday: '11:00AM - 6:00PM',
     thursday: '11:00AM - 6:00PM',
@@ -40,11 +40,11 @@ const hours = {
 };
 
 // Set season here
-export const currentHours = hours.winter;
+export const currentHours = hours.spring;
 
 // assign hours for today
 const hoursMap = {
-  1: currentHours.Monday,
+  1: currentHours.monday,
   2: currentHours.tuesday,
   3: currentHours.wednesday,
   4: currentHours.thursday,
