b3b0d608f4351b09afc13743a6ee507ee60a97ce
Nick Cacace <NickCacace@gmail.com>
2026-01-06 11:32:01 -0500

    build: upgrade development dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,12 @@
-import { defineConfig } from 'eslint/config'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-import js from '@eslint/js'
-import { FlatCompat } from '@eslint/eslintrc'
+import nextTypescriptConfig from 'eslint-config-next/typescript'
+import nextCoreWebVitalsConfig from 'eslint-config-next/core-web-vitals'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-})
-
-export default defineConfig([
+const eslintConfig = [
+  ...nextCoreWebVitalsConfig,
+  ...nextTypescriptConfig,
   {
-    extends: compat.extends('next/core-web-vitals', 'next/typescript'),
     ignores: ['node_modules/**', 'build/**', '.next/**'],
   },
-])
+]
+
+export default eslintConfig
